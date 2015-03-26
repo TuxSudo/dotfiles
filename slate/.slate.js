3d0aa28dc164bb589ba1c97f2c4fdee7bc037e2d
Jared Anderson <jared@tuxsudo.com>
2015-03-26 08:40:57 -0600

    added slate preferences

---
+++ slate/.slate.js
@@ -0,0 +1,42 @@
+var fullscreen = slate.operation("move", {
+    "x": "screenOriginX",
+    "y": "screenOriginY",
+    "width": "screenSizeX",
+    "height": "screenSizeY"
+});
+
+var pushRight = slate.operation("push", {
+    "direction": "right",
+    "style": "bar-resize:screenSizeX/2",
+});
+
+var pushLeft = slate.operation("push", {
+    "direction": "left",
+    "style": "bar-resize:screenSizeX/2"
+});
+
+var pushTop = slate.operation("push", {
+    "direction": "top",
+    "style": "bar-resize:screenSizeY/2"
+});
+
+var pushBottom = slate.operation("push", {
+    "direction": "bottom",
+    "style": "bar-resize:screenSizeY/2"
+});
+
+
+
+
+
+// Bind A Crazy Function to 1+ctrl
+slate.bnda({
+
+	"f:cmd,shift,space": fullscreen,
+	"up:cmd,shift,space": pushTop,
+	"left:cmd,shift,space": pushLeft,
+	"right:cmd,shift,space": pushRight,
+	"down:cmd,shift,space": pushBottom
+
+
+});
